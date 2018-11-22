1a307f6f2544c73fd10f32ff8d3a0e12a4480d1c
Stuart Lee <lee.s@wehi.edu.au>
2018-11-23 10:09:27 +1100

    add convex hull of points, widget controls

--- R/plot-method.R
+++ R/plot-method.R
@@ -1,5 +1,3 @@
-
-#' @rdname plot.nearby
 #' @method plot nearby 
 #' @import leaflet
 #' @export
@@ -8,15 +6,25 @@ plot.nearby <- function(x, y, ...) {
     stop("Not valid for plot...")
   }
   
+  hpts <- grDevices::chull(x$amenities[, c("x", "y")])
+  
   leaflet() %>% 
     addTiles() %>% 
     fitBounds(lng1 = x$bounding_box[1,1],
               lat1 = x$bounding_box[2,1],
               lng2 = x$bounding_box[1,2],
               lat2 = x$bounding_box[2,2]) %>% 
-    addCircleMarkers(lng = x$amenities[["x"]],
-                     lat = x$amenities[["y"]],
-                     label = x$amenities[["type"]]
-                     )
+    addCircles(lng = x$amenities[["x"]],
+               lat = x$amenities[["y"]],
+               group = x$amenities[["type"]],
+               radius = 0.2,
+               opacity = 0.5,
+               color = "black"
+               
+    ) %>% 
+    addPolygons(lng = x$amenities[hpts, "x"],
+                lat = x$amenities[hpts, "y"]
+                ) %>% 
+    addLayersControl(overlayGroups = unique(x$amenities[["type"]]))
   
 }
