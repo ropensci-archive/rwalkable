287d668aea5f6fc77dd9d85bd254619c92f86211
Thomas Lumley <t.lumley@auckland.ac.nz>
2018-11-22 16:59:58 +1100

    distances with dodgr and graph compaction

--- R/metrics.R
+++ R/metrics.R
@@ -1,11 +1,9 @@
 ## number of intersections per unit area was used by the Canadians
 ## https://www.ncbi.nlm.nih.gov/pubmed/24454837
 ##
-## adhesion seems a useful additional summary
 
 get_graph_metrics<-function(graph){
   ## We're going to compact the dodgr graph and then count the vertices.
-  degree<- sum(igraph::degree(graph)>2)
-  adhesion<-igraph::adhesion(graph)
-  list(degree=degree,cohesion=adhesion)
+  graph<-dodgr::dodgr_compact_graph(graph)$graph
+  nrow(graph)-nrow(dodgr::dodgr_vertices(graph))
 }
