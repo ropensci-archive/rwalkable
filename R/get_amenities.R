0937d16e231a224e8e460188d6595e701833c078
Stuart Lee <lee.s@wehi.edu.au>
2018-11-22 12:20:37 +1100

    first pass at amenities

--- R/get_amenities.R
+++ R/get_amenities.R
@@ -1,22 +1,30 @@
 #' Retrieve amenities for a neighbourhood
 #' 
 #' 
-#' @param bbox bounding box either four numeric values or a character string
-#' as passed to [osmdata::opq()].
+#' @param location A character string or bounding box as passed to [osmdata::opq()].
+#' 
+#' @param ... additional arguments to pass to [osmdata::opq()].
 #' 
 #' @param amenities a character vector representing amentities to add as
 #' defined by [Open Street Map](https://wiki.openstreetmap.org/wiki/Key:amenity). 
-#' If missing will default to 
+#' If NULL will default to all amenity tags. 
 #' 
-#'     
-#' 
-get_amenities <- function(bbox, amenities = NULL) {
+#' @return an sf Simple Features Collection of points
+#'         
+#' @importFrom osmdata opq add_osm_feature
+get_amenities <- function(location, ..., amenities = NULL) {
   
-  query <- osmdata::opq(bbox)
+  query <- opq(location, ...)
   
-  if (is.null(include)) {
-    query %>% 
+  if (is.null(amenities)) {
+    query <- query %>% 
       add_osm_feature("amenity")
+  } else {
+    # add_osm_feature is not vectorised in value, so we will loop over it
+    for (i in seq_along(amenities)) {
+      query <- query %>% add_osm_feature("amenity", amenities[[i]])
+    }
   }
-    
+  
+  osmdata_sf(query)$osm_points
 }
