42877510d632cd5c6ed2e899fa77903034f69bef
Thomas Lumley <t.lumley@auckland.ac.nz>
2018-11-22 15:30:02 +1100

    graph metrics: degree, adhesion

---
+++ R/metrics.R
@@ -0,0 +1,10 @@
+## number of intersections per unit area was used by the Canadians
+## https://www.ncbi.nlm.nih.gov/pubmed/24454837
+##
+## adhesion seems a useful additional summary
+
+get_graph_metrics<-function(graph){
+  degree<- sum(igraph::degree(graph)>2)
+  adhesion<-igraph::adhesion(graph)
+  list(degree=degree,cohesion=adhesion)
+}
