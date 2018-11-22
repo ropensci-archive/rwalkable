44ecfd5cd1c5b470876cf9752165a0b75fc21350
Stuart Lee <lee.s@wehi.edu.au>
2018-11-23 10:22:31 +1100

    document plot method

--- R/plot-method.R
+++ R/plot-method.R
@@ -1,9 +1,23 @@
+#' Produce a map of nearby amenities
+#' 
 #' @method plot nearby 
+#' 
+#' @param x An object of class `nearby`
+#' @param y  Not used for `plot.nearby`
+#' @param ... Additional arguments to plot, currently does not do anything
+#' 
+#' @details The plot method produces a map of nearby amenities, with
+#' a convex hull over the amenities' longitude and latitude. There is
+#' a dropdown menu on the top right, to filter by by an amenity type. 
+#' 
+#' 
+#' @return a Leaflet map widget 
+#' 
 #' @import leaflet
 #' @export
 plot.nearby <- function(x, y, ...) {
   if (!missing(y)) {
-    stop("Not valid for plot...")
+    stop("Not valid for plot.nearby...")
   }
   
   hpts <- grDevices::chull(x$amenities[, c("x", "y")])
