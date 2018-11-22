1b5719250974e3911c7f983accecf3298cbcd775
Thomas Lumley <t.lumley@auckland.ac.nz>
2018-11-22 13:59:04 +1100

    adds whereis function

--- R/whereis.R
+++ R/whereis.R
@@ -2,8 +2,16 @@
 #' @title Look up coordinates.
 #' @description Look up latitude and longitude
 #' @export
+#' @examples 
+#'   whereis("Melbourne")
+#'   whereis("Onehunga, Auckland, New Zealand")
+#'   whereis("Love, OK")
 
-whereis <-function(place){
-  ## do it
+whereis <-function(place, quietly=FALSE){
+  bb<-osmdata::getbb(place)
+  if(is.na(bb[1,1]) && !quietly)
+    stop(paste("can't find",place,"on OpenStreetMap."))
   
+  return(list(latitude=mean(bb["y",]),
+              longitude=mean(bb["x",])))
 }
