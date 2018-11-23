f8674da82d12ee5d0b21ababd1fac59345fdc4bb
apear9 <alan.pearse@connect.qut.edu.au>
2018-11-23 12:04:59 +1000

    Working version of get_roads_isochrone

---
+++ R/get_roads_isochrone.R
@@ -0,0 +1,65 @@
+#' Get buffered road network
+#'
+#' @description
+#'
+#' Get polygons representing the roads leading to the accessible amenities returned by \code{nearby()}
+#'
+#' @param nearby_obj An object of class \code{nearby}.
+#' @param radius The maximum distance to travel along the road network.
+#' @param buffer_dist A buffer distance in decimal degrees. Defaults to 0.0002 (~20 metres).
+#' @return An object of class sf containing polygonal features representing the parts of the road network that lead to accessible amenities.
+get_roads_isochrone <- function(nearby_obj, radius, buffer_dist = 0.0002){
+
+  # Location
+
+  location <- nearby_obj$location
+
+  # Get location to trace road network from
+  location_xy <-if(is.character(location)) {
+    whereis(location)
+  } else if (inherits(location,"POINT")){
+    location<-as.numeric(location)
+  } else if (inherits(location,"MULTIPOINT")){
+    location<-colMeans(location)
+  } else location
+
+  delta_lat<-radius_to_latitude(radius)
+  delta_long<-radius_to_longitude(radius, location_xy["latitude"])
+
+  location_bb<-  matrix(c(location_xy["longitude"]-delta_long,
+                          location_xy["longitude"]+delta_long,
+                          location_xy["latitude"]-delta_lat,
+                          location_xy["latitude"]+delta_lat),
+                        ncol=2, byrow=TRUE)
+  colnames(location_bb)<-c("min","max")
+  rownames(location_bb)<-c("x","y")
+
+
+  # Get bbox for extracting roads
+  road_bbx <- location_bb
+
+  # Get roads as dodgr graph
+  road_net <- get_roads_graph(road_bbx)
+
+  # Find subset of these which are within walking distance of the specified location
+  goto_mat_x <- rowMeans(cbind(road_net$to_lon, road_net$from_lon))
+  goto_mat_y <- rowMeans(cbind(road_net$to_lat, road_net$from_lat))
+  goto_mat <<- cbind(goto_mat_x, goto_mat_y)
+  colnames(goto_mat) <- c("x", "y")
+  subs_ind <- is_nearby(goto_mat, location_xy, road_net, radius)
+  subs_ind <<- subs_ind[1, ]
+
+  # Subset road network
+  geom_nums <<- unique(road_net$geom_num[subs_ind])
+  road_net <- road_net[which(road_net$geom_num %in% geom_nums), ]
+
+  # Coerce to class sf
+  road_sfc <- dodgr::dodgr_to_sfc(road_net)$geoms
+
+  # Buffer by fixed distance of 0.0002 arc-degrees
+  road_buf <- tryCatch(sf::st_buffer(road_sfc, buffer_dist), error = function(e) stop("Cannot buffer"))
+
+  # Return
+  return(road_buf)
+
+}
